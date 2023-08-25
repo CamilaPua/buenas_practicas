827e575221b635a2f1f91adc8141243cad631e28
Sergio Esteban Alfonso <andresmt1119@gmail.com>
2023-08-25 16:14:32 -0500

    agrega pequeña consulta a la base de datos

--- connection.py
+++ connection.py
@@ -1,12 +1,25 @@
 import sys
 import pyodbc
 
-conn = pyodbc.connect(r"Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=C:\Users\cuc\buenas_practicas\db_prueba.accdb;")
-cursor = conn.cursor()
-cursor.execute("SELECT * FROM personas")
+def run():
+    conn = pyodbc.connect(r"Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=C:\Users\cuc\buenas_practicas\db_prueba.accdb;")
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM personas")
 
-for row in cursor.fetchall():
-    print(row)
+    for row in cursor.fetchall():
+        print('-'*30 + f'\nID: {row[0]}\nNombre:{row[1]}\nEdad:{row[2]}\n' + '-'*30)
 
-cursor.close
-conn.close
+
+    print('\nPersonas menores o con 18 años')
+
+    cursor.execute("SELECT * FROM personas WHERE edad <= 18")
+    for row in cursor.fetchall():
+        print('-'*30 + f'\nID: {row[0]}\nNombre:{row[1]}\nEdad:{row[2]}\n' + '-'*30)
+
+
+    cursor.close
+    conn.close
+
+
+if __name__ == '__main__':
+    run()
